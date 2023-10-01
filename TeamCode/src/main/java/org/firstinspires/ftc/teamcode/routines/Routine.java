283c3257818e969da309413887057f7f42be0826
TerminalVibes <ominousvibes.exploit@gmail.com>
2023-09-30 22:51:39 -0400

    More infrastructure

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/routines/Routine.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode.routines;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class Routine extends LinearOpMode {
+	public ExecutorService executor = Executors.newFixedThreadPool(4);
+	private boolean exited = false;
+
+	@Override
+	public void runOpMode() throws InterruptedException {
+		// Run initialization code
+		onInit();
+
+		// Wait for start in case of initialization code ending early
+		waitForStart();
+		if (isStopRequested()) { // Terminate code if cancelled
+			onExit();
+			return;
+		}
+
+		// Run routine code
+		executor.submit(() -> {
+			onStart();
+			if (!this.exited) onExit();
+		});
+
+		// Handle unexpected exits
+		do sleep(100);
+		while (opModeIsActive() && !this.exited);
+		if (!this.exited) onExit();
+	}
+
+	public void onInit() {}
+	public void onStart() {}
+	public void onExit() {
+		this.exited = true;
+		executor.shutdownNow();
+	}
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Subsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Subsystem.java
@@ -1,11 +1,22 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
-import org.firstinspires.ftc.teamcode.routine.Routine;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.routines.Routine;
 
 public abstract class Subsystem {
 	protected Routine routine;
+	protected Telemetry telemetry;
 
 	public Subsystem(Routine routine) {
 		this.routine = routine;
+		this.telemetry = routine.telemetry;
+	}
+
+	public void onInit() {}
+
+	public void onStart() {}
+
+	protected boolean isStopRequested() {
+		return routine.isStopRequested();
 	}
 }
