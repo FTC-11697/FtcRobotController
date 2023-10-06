84167d3ef0e31d7bcdd3e2e8e3075dcb473dd491
LL-Studio <81865868+LL-Studio@users.noreply.github.com>
2023-10-06 19:44:44 -0400

    Finished Routines

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/routines/driver/DriverRoutine.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/routines/driver/DriverRoutine.java
@@ -1,4 +1,23 @@
 package org.firstinspires.ftc.teamcode.routines.driver;
 
-public class DriverRoutine {
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.routines.Routine;
+
+@TeleOp(name = "Driver")
+public class DriverRoutine extends Routine {
+	@Override
+	public void onInit() {
+		super.onInit();
+	}
+
+	@Override
+	public void onStart() {
+		super.onStart();
+	}
+
+	@Override
+	public void onExit() {
+		super.onExit();
+	}
 }
