75695e9651cf84b31ab91cb1fda70bf4d7697541
LL-Studio <81865868+LL-Studio@users.noreply.github.com>
2023-10-06 19:52:13 -0400

    Routine Relationship

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/routines/driver/DriverRoutine.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/routines/driver/DriverRoutine.java
@@ -3,9 +3,12 @@ package org.firstinspires.ftc.teamcode.routines.driver;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.routines.Routine;
+import org.firstinspires.ftc.teamcode.subsystems.System;
 
 @TeleOp(name = "Driver")
 public class DriverRoutine extends Routine {
+	public System system = new System(this);
+
 	@Override
 	public void onInit() {
 		super.onInit();
